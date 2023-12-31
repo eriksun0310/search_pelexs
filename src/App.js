b9844dc9c57f55db53da407f59bb2d2e67c4dd2e
eriksun0310 <erikawang1233@gmail.com>
2023-12-31 21:40:30 +0800

    不用BrowserRouter

--- src/App.js
+++ src/App.js
@@ -4,36 +4,39 @@ import Layout from "./Layout";
 import Homepage from "./pages/Homepage";
 import About from "./pages/About";
 import Page404 from "./pages/Page404";
+import Footer from "./components/Footer";
 import "./styles/style.css"
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 
 function App() {
   return (
-    // <div>
-    //     <nav>
-    //         <ul>
-    //             <li>
-    //               <a href="/">
-    //                 <i class="bi bi-house-door" style={{fontSize:'29px'}}/>
-    //               </a>
-    //             </li>
-    //         </ul>
-    //     </nav>
-    //     <Homepage></Homepage>
-    //     {/* <Outlet/>
-    //     <Footer /> */}
-    // </div>
+    <div>
+        <nav>
+            <ul>
+                <li>
+                  <a href="/">
+                    <i class="bi bi-house-door" 
+                    style={{fontSize:'29px'}}
+                    />
+                  </a>
+                </li>
+            </ul>
+        </nav>
+        <Homepage/>
+        <Footer/>
+    </div>
 
   
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Homepage/>} />
-          <Route path="about" element={<About/>}/>
-        </Route>
-      </Routes>   
-    </BrowserRouter>
+    // 不知道為什麼上傳到gitHub 畫面出不來
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Layout/>}>
+    //       <Route index element={<Homepage/>} />
+    //       <Route path="about" element={<About/>}/>
+    //     </Route>
+    //   </Routes>   
+    // </BrowserRouter>
   );
 }
 
